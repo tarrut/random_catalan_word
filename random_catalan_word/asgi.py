7a4f2921f33df731041bb05ad68350afb7d7a00d
tarrut <91079521+tarrut@users.noreply.github.com>
2025-09-26 23:59:44 +0200

    chore: start django

---
+++ random_catalan_word/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for random_catalan_word project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_catalan_word.settings')
+
+application = get_asgi_application()
---
+++ random_catalan_word/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for random_catalan_word project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_catalan_word.settings')
+
+application = get_wsgi_application()
